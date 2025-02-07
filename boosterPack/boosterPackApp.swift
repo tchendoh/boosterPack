0b7762aad6fc5bb5373f0b7319a95c39fd05c4fc
Eric Chandonnet <tchendoh@gmail.com>
2025-02-07 09:46:17 -0500

    Initial Commit

---
+++ boosterPack/boosterPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  boosterPackApp.swift
+//  boosterPack
+//
+//  Created by Eric Chandonnet on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct boosterPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
