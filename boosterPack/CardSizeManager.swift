2a0ce11ba4f8791664ed2cb1912b64e2124f3fc0
Eric Chandonnet <tchendoh@gmail.com>
2025-02-08 10:45:36 -0500

    Add .gitignore and swift rules.

--- boosterPack/CardSizeManager.swift
+++ boosterPack/CardSizeManager.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+class CardSizeManager {
+    var cardWidth: CGFloat
+    let realWidth: CGFloat = 2.5 // real physical card in 2.5 in / 3.5 in
+    let realHeight: CGFloat = 3.5
+    let realBleedSize: CGFloat = 0.078740 // 2mm (25.4mm = 1 inch)
+    let realCardCornerRadius: CGFloat = 0.125
+    let safeCardCornerRadius: CGFloat = 0.11
+    var cardAspectRatio: CGFloat {
+        realHeight / realWidth
+    }
+    var cardHeight: CGFloat {
+        cardWidth * realHeight / realWidth
+    }
+    var safeWidth: CGFloat {
+        cardWidth - (widthBleedSize * 2)
+    }
+
+    var safeHeight: CGFloat {
+        cardHeight - (heightBleedSize * 2)
+    }
+
+    var widthBleedSize: CGFloat {
+        realBleedSize / realWidth * cardWidth
+    }
+    var heightBleedSize: CGFloat {
+        realBleedSize / realHeight * cardHeight
+    }
+    
+    var cardCornerRadius: CGFloat {
+        realCardCornerRadius / realWidth * cardWidth
+    }
+
+    var safeCornerRadius: CGFloat {
+        safeCardCornerRadius / realWidth * cardWidth
+    }
+
+    init(cardWidth: CGFloat = 300) {
+        self.cardWidth = cardWidth
+    }
+}
--- boosterPack/ContentView.swift
+++ boosterPack/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    var cardSize = CardSizeManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            let cardSize = CardSizeManager(cardWidth: geo.size.width * 0.9)
+            
+            HStack {
+                Spacer()
+                ZStack {
+                    Rectangle()
+                        .frame(width: cardSize.cardWidth, height: cardSize.cardHeight, alignment: .center)
+                        .foregroundStyle(.yellow)
+                        .cornerRadius(cardSize.cardCornerRadius)
+                    Rectangle()
+                        .frame(width: cardSize.safeWidth, height: cardSize.safeHeight, alignment: .center)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [.red, .blue],
+                                startPoint: .topLeading,
+                                endPoint: .trailing
+                            ))
+                        .cornerRadius(cardSize.safeCornerRadius)
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
